a8b179b42478b380bbe18074cdb3c8a9b442c5c1
guizhiming <guizhiming@hz-cpp.com>
2022-06-08 17:54:33 +0800

    Initial commit

---
+++ src/main/java/org/nirvana/JavaWebmagicApplication.java
@@ -0,0 +1,13 @@
+package org.nirvana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebmagicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebmagicApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/nirvana/service/ReptileService.java
@@ -0,0 +1,14 @@
+package org.nirvana.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 爬虫服务类
+ */
+@Service
+public class ReptileService {
+
+    public void reptile(String url) {
+
+    }
+}
